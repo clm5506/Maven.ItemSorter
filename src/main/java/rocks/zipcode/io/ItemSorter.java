d1f8923fe849e861ca03b3fe380e77151c301282
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-02-19 19:47:20 -0500

    finalized itemsorter

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,27 @@
 package rocks.zipcode.io;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    Item[] items;
+
+    //Constructor
     public ItemSorter(Item[] items) {
+
+        this.items = items;
+
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(items,comparator);
+
+        return items;
     }
 }
--- src/test/java/rocks/zipcode/io/itemsorter/SortByPriceTest.java
+++ src/test/java/rocks/zipcode/io/itemsorter/SortByPriceTest.java
@@ -1,7 +1,68 @@
 package rocks.zipcode.io.itemsorter;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcode.io.Item;
+import rocks.zipcode.io.ItemSorter;
+import rocks.zipcode.io.comparators.IdComparator;
+import rocks.zipcode.io.comparators.PriceComparator;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
 public class SortByPriceTest {
+
+    @Test
+
+    public void sortByPriceTest1() {
+        Item item3 = new Item(3L, "Apple", 1.0);
+        Item item2 = new Item(4L, "Banana", 1.5);
+        Item item1 = new Item(5L, "Cherry", 2.0);
+        Item[] itemsToSort = {item2, item1, item3};
+        Item[] expected = {item3, item2, item1};
+        ItemSorter itemSorter = new ItemSorter(itemsToSort);
+        Comparator<Item> comparator = (Comparator<Item>) new PriceComparator();
+
+        // when
+        Item[] actual = itemSorter.sort(comparator);
+
+        // then
+        Assert.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void sortByPriceTest2() {
+        Item item3 = new Item(3L, "Apple", 3.0);
+        Item item2 = new Item(4L, "Banana", 1.5);
+        Item item1 = new Item(5L, "Cherry", 2.0);
+        Item[] itemsToSort = {item2, item1, item3};
+        Item[] expected = {item2,item1, item3};
+        ItemSorter itemSorter = new ItemSorter(itemsToSort);
+        Comparator<Item> comparator = (Comparator<Item>) new PriceComparator();
+
+        // when
+        Item[] actual = itemSorter.sort(comparator);
+
+        // then
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void sortByPriceTest3() {
+        Item item3 = new Item(3L, "Apple", 1.0);
+        Item item2 = new Item(4L, "Banana", 1.5);
+        Item item1 = new Item(5L, "Cherry", 2.0);
+        Item[] itemsToSort = {item2, item1, item3};
+        Item[] expected = {item3, item2, item1};
+        ItemSorter itemSorter = new ItemSorter(itemsToSort);
+        Comparator<Item> comparator = (Comparator<Item>) new PriceComparator();
+
+        // when
+        Item[] actual = itemSorter.sort(comparator);
+
+        // then
+        Assert.assertArrayEquals(expected, actual);
+    }
+
 }
